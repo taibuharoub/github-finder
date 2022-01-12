a412bf32922151593e101612daab07d2d5c46a00
Taibu Haroub <haroubtaibu@gmail.com>
2022-01-12 23:06:37 +0300

    Add pages and routes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
 import Footer from "./components/layout/Footer";
+import About from "./pages/About";
+import Home from "./pages/Home";
+import NotFound from "./pages/NotFound";
+
+// lazy loading
+// const About = React.lazy(() => import("./pages/About"));
+// const Home = React.lazy(() => import("./pages/Home"));
+// const NotFound = React.lazy(() => import("./pages/NotFound"));
 
 function App() {
   return (
     <Router>
       <div className="flex flex-col justify-between h-screen">
         <Navbar />
-        <main className="container mx-auto px-3 pb-12">Content</main>
+        <main className="container mx-auto px-3 pb-12">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/notfound" element={<NotFound />} />
+            <Route path="/*" element={<NotFound />} />
+          </Routes>
+        </main>
         <Footer />
       </div>
     </Router>
